b94112ca67f108a3de6b14a113f6abbe88a610ff
cbraz <celso.braz@gmail.com>
2014-04-13 19:42:31 +0100

    Update FileMD.py

--- FileMD.py
+++ FileMD.py
@@ -1,10 +1,10 @@
-class FileData:
+class FileMD:
     def __init__(self, p):
         self.path = p
         self.filesize = 0
         self.filehash = 0
-#    def __str__(self):
-#        return self.path
+    def __str__(self):
+        return self.path
    
     def to_json(self):
         return self.__dict__
